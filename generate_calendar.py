db70c65d5efea4120040155d7d6d0ed00f2675b3
sagarvadalia87 <sagar.bau@gmail.com>
2026-06-27 11:43:43 +0530

    Update generate_calendar.py

--- generate_calendar.py
+++ generate_calendar.py
@@ -1,1 +1,10 @@
+mport pandas as pd
 
+SHEET_ID = "1vOgaCYYH2EGlBC4hm_lWaQ5yFDzky6-ZqNni61DbkPc"
+GID = "593313266"
+
+url = f"https://docs.google.com/spreadsheets/d/{SHEET_ID}/export?format=csv&gid={GID}"
+
+df = pd.read_csv(url)
+
+print(df.head())
